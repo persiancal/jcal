988f5b56e39da47d00c965ee215ce04bc8bf169a
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2025-04-03 21:14:37 +0330

    Format autogen and add guide on it

--- sources/CONTRIBUTION.md
+++ sources/CONTRIBUTION.md
@@ -2,3 +2,4 @@
 
 It is suggested for the new contributors to use the following tools:
 - `clang-format` for all the C files, source and headers.
+- `shfmt -ci -i 4 -ln posix` for shell format with the following parameters.
--- sources/autogen.sh
+++ sources/autogen.sh
@@ -7,20 +7,20 @@
 
 libtoolize="$(command -v libtoolize || command -v glibtoolize)"
 if [ -z "$libtoolize" ]; then
-	printf "libtoolize or glibtoolize is needed and not found.
+    printf "libtoolize or glibtoolize is needed and not found.
 		Add to \$PATH or use another way to bootstrap the project.\n"
-	exit 1
+    exit 1
 fi
 
 "$libtoolize" --force --copy --install || exit 1
 if command -v autoreconf; then
-	printf 'using autoreconf...\n'
-	autoreconf --force --install || exit 1
-	automake --add-missing --force-missing --copy || exit 1
-	autoconf --force || exit 1
+    printf 'using autoreconf...\n'
+    autoreconf --force --install || exit 1
+    automake --add-missing --force-missing --copy || exit 1
+    autoconf --force || exit 1
 else
-	printf 'not using autoreconf...\n'
-	autoreconf --force --install || exit 1
-fi	
+    printf 'not using autoreconf...\n'
+    autoreconf --force --install || exit 1
+fi
 
 printf "done. Read the README and INSTALL files.\n"
