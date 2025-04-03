084e554735c7aeacfb2f1197ecfbc8c2071bb9ba
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2025-04-03 21:12:19 +0330

    Remove alternative flag
    Since there are no other flags, also remove --help.

--- sources/autogen.sh
+++ sources/autogen.sh
@@ -20,60 +20,8 @@
 # along with libjalali.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# @OPTIONS
-OPTS="ah"
-LONG_OPTS="help,alternative"
-
-# @USAGE
-function usage() {
-	echo -e "Jalali calendar library autogen build script."
-	echo -e "usage: autogen.sh [-ch]"
-	echo -e "try \`autogen.sh --help\' for more information."
-}
-
-# @HELP
-function help() {
-	echo -e "usage: autogen.sh [-ch]..."
-	echo -ne "Invokes GNU build system tools in order to create"
-	echo -e " necessary configuration scripts.\n"
-	echo -e "Operation modes:"
-	echo -e "  -a, --alternative\tdo not invoke autoreconf"
-	echo -e "  -h, --help\t\tprint this help, then exit\n"
-	echo -e "Report bugs to <ghassemi@ftml.net>."
-	echo -e "Jalali calendar home page: <http://nongnu.org/jcal>."
-}
-
-# Operation modes.
-HELP=0
-ALTERN=0
-
-which which 1>/dev/null 2>&1
-if [ $? -ne 0 ]; then
-	echo -e "cannot find \`\`which''. autogen cannot continue."
-	exit 1
-fi
-
-# Parsing command-line arguments
-GETOPT=`which getopt 2>/dev/null`
-if [ -z ${GETOPT} ]; then
-	echo -ne "warning: getopt(1) was not found on your system."
-	echo -e " command line arguments will be ignored."
-else
-	TEMP=`${GETOPT} -o ${OPTS} -l ${LONG_OPTS} -n 'autogen.sh' -- "$@"`
-
-	for i in $TEMP; do
-		case $i in
-			-h|--help) let HELP=1;;
-			-a|--alternative) let ALTERN=1;;
-		esac
-	done
-fi
-
-# HELP
-if [ ${HELP} -eq 1 ]; then
-	help
-	exit 0
-fi
+# Reverse the generation by removing any file created (`git clean -fxdn` lists
+# them).
 
 libtoolize="$(command -v libtoolize || command -v glibtoolize)"
 if [ -z "$libtoolize" ]; then
@@ -83,7 +31,7 @@ if [ -z "$libtoolize" ]; then
 fi
 
 "$libtoolize" --force --copy --install || exit 1
-if command -v autoreconf || [ "$ALTERN" = "1" ]; then
+if command -v autoreconf; then
 	printf 'using autoreconf...\n'
 	autoreconf --force --install || exit 1
 	automake --add-missing --force-missing --copy || exit 1
