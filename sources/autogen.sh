57cdccb44dbbb027987f949bd4876485da4ed367
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2025-04-03 21:13:06 +0330

    Remove the extra documentation comments in autogen

--- sources/autogen.sh
+++ sources/autogen.sh
@@ -1,25 +1,7 @@
 #!/usr/bin/env bash
 #
-# autogen.sh - Tools for manipulating Jalali representation of Iranian calendar
-# and necessary conversations to Gregorian calendar.
-# Copyright (C) 2006, 2007, 2009, 2010, 2011 Ashkan Ghassemi.
+# Run GNU Autotool commands forcefully to regenerate stuff.
 #
-# This file is part of libjalali.
-#
-# libjalali is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# libjalali is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with libjalali.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 # Reverse the generation by removing any file created (`git clean -fxdn` lists
 # them).
 
