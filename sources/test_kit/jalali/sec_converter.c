56dd51ef189f06b385f5289937e967b4139580d9
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2025-04-05 23:43:43 +0330

    Format all the .c and .h files

--- sources/test_kit/jalali/sec_converter.c
+++ sources/test_kit/jalali/sec_converter.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "jalali.h"
 
-int main(int argc, char** argv)
-{
-    if (argc != 2) {
+int main(int argc, char **argv) {
+  if (argc != 2) {
     printf("wrong arguments given\n");
     printf("usage: sec_converter SECONDS\n");
     exit(1);
-    }
+  }
 
-    struct ab_jtm a;
-    int s;
-    s = atoi(argv[1]);
-    jalali_create_time_from_secs(s, &a);
-    printf("%d secs passed, time created: %d days, %02d:%02d:%02d.\n", s, a.ab_days, a.ab_hour, a.ab_min, a.ab_sec);
+  struct ab_jtm a;
+  int s;
+  s = atoi(argv[1]);
+  jalali_create_time_from_secs(s, &a);
+  printf("%d secs passed, time created: %d days, %02d:%02d:%02d.\n", s,
+         a.ab_days, a.ab_hour, a.ab_min, a.ab_sec);
 
-    exit(0);
+  exit(0);
 }
--- sources/test_kit/jtime/jgmtime.c
+++ sources/test_kit/jtime/jgmtime.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <time.h>
+
 #include "jalali.h"
 #include "jtime.h"
 
 int main() {
-    time_t t;
-    time(&t);
-    struct jtm j;
-    struct jtm* p;
-    p = jgmtime(&t);
-    jalali_show_time(p);
-    jgmtime_r(&t, &j);
-    jalali_show_time(&j);
-    return 0;
+  time_t t;
+  time(&t);
+  struct jtm j;
+  struct jtm *p;
+  p = jgmtime(&t);
+  jalali_show_time(p);
+  jgmtime_r(&t, &j);
+  jalali_show_time(&j);
+  return 0;
 }
